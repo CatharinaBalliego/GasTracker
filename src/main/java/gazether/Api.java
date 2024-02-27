bec771312a7ed3a3b19906281e5a2d2f90535771
Catharina <catharina_@hotmail.com.br>
2024-02-27 15:34:49 -0300

    adiciona chamada da API

--- src/main/java/gazether/Api.java
+++ src/main/java/gazether/Api.java
@@ -1,10 +1,45 @@
 package gazether;
 
 import java.net.URI;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import com.google.gson.Gson;
+
+import gazether.Model.Gas;
 
 public class Api {
 
-	
-	//https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=YourApiKeyToken
+	public String getGwei() {
+		try {	
+			Gas gas = new Gas();
+			Gson gson = new Gson();
+			HttpRequest request = HttpRequest.newBuilder()
+					.uri(new URI("https://api.etherscan.io/api?module=gastracker&action=gasoracle&apiKey=B9ZVBJE4ZD94RBVM678Y2NBK9YQPUJJDWT"))
+					.build();
+			
+			HttpClient httpClient = HttpClient.newHttpClient();
+
+			HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
+			
+			if(response.statusCode() != 200) {
+				//wait
+				//continue;
+				return "Erro ao conectar com a API!";
+			}
+			
+			else {
+				gas = gson.fromJson(response.body(), Gas.class);
+				System.out.println(gas.getStatus());
+				return response.body();
+			}
+			
+		} catch(Exception e) {
+			System.out.println("erro: " + e.getLocalizedMessage());
+		}
+		return "error";
+		
+	}
 }
